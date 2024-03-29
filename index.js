d943746df2c5fd5411d95d4008b687ae1c96f08b
Saptadeep Banerjee <69459134+imSanko@users.noreply.github.com>
2024-03-29 07:25:23 +0530

    Revert "Update index.js"
    This reverts commit 152207b77cab8be8708c1d037adca11c8145aa7b.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const aoijs = require("aoi.js");
-const bot = new aoijs.AoiClient
-({
+const bot = new aoijs.AoiClient({
   token: process.env.Token,
   prefix: "?",
   intents: ["GUILDS", "GUILD_MESSAGES"],
